4b5837469c53bc3f7cfce161a4bfee362d28397e
Mariana Guanda <mariana@fleetflex.io>
2024-11-20 23:02:30 -0400

    update values

--- src/index.js
+++ src/index.js
@@ -30,6 +30,8 @@ function displayTemperature(response) {
 
     let timeElement = document.querySelector("#current-date");
     timeElement.innerHTML = formatDate(date);
+
+    getForecast(response.data.city);
 }
 
 //Form
@@ -72,6 +74,48 @@ function formatDate(date) {
     return `${formattedDay} ${hours}:${minutes}`;
 }
 
+function formatDay(timestamp) {
+    let date = new Date(timestamp * 1000);
+    let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+    return days[date.getDay()];
+}
+
+function displayForecast(response) {
+    let forecastHtml = "";
+
+    response.data.daily.forEach(function (day, index) {
+        if (index < 5) {
+            forecastHtml =
+                forecastHtml +
+                `
+      <div class="weather-forecast-day">
+        <div class="weather-forecast-date">${formatDay(day.time)}</div>
+
+        <img src="${day.condition.icon_url}" class="weather-forecast-icon" />
+        <div class="weather-forecast-temperatures">
+          <div class="weather-forecast-temperature">
+            <strong>${Math.round(day.temperature.maximum)}º</strong>
+          </div>
+          <div class="weather-forecast-temperature">${Math.round(
+                    day.temperature.minimum
+                )}º</div>
+        </div>
+      </div>
+    `;
+        }
+    });
+
+    let forecastElement = document.querySelector("#forecast");
+    forecastElement.innerHTML = forecastHtml;
+}
+
+function getForecast(city) {
+    let apiKey = "b2a5adcct04b33178913oc335f405433";
+    let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units=metric`;
+    axios(apiUrl).then(displayForecast);
+}
+
 let searchForm = document.querySelector("#search-form");
 searchForm.addEventListener("submit", search);
 
@@ -79,3 +123,5 @@ let currentDateELement = document.querySelector("#current-date");
 let currentDate = new Date();
 
 currentDateELement.innerHTML = formatDate(currentDate);
+
+getForecast("Paris");
